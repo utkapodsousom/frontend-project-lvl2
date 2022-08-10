358fb8fcd9165e58a48d9fd8a9619589521d15c4
utkapodsousom <utkapodsousom@gmail.com>
2022-08-10 20:08:02 +0300

    add lodash

--- gendiff.js
+++ gendiff.js
@@ -1,5 +1,7 @@
 const { Command, Option } = require('commander');
 const program = new Command();
+const _ = require('lodash');
+import * as fs from 'node:fs';
 
 program.argument('<filepath1>', '').argument('<filepath2>', '');
 
