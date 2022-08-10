de58516aa926890ff1911989374753549c708ec7
utkapodsousom <utkapodsousom@gmail.com>
2022-08-10 18:35:17 +0300

    add basic usage info, add gitignore

---
+++ gendiff.js
@@ -0,0 +1,8 @@
+const { Command, Option } = require('commander');
+const program = new Command();
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .option('-V, --version', 'output the version number');
+
+program.parse();
