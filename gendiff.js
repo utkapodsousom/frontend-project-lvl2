1c3b45c835b5317cc39290f3cf2184918555d8fd
utkapodsousom <utkapodsousom@gmail.com>
2022-08-10 19:57:03 +0300

    modify usage info

--- gendiff.js
+++ gendiff.js
@@ -1,8 +1,12 @@
 const { Command, Option } = require('commander');
 const program = new Command();
 
+program.argument('<filepath1>', '').argument('<filepath2>', '');
+
 program
   .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number');
+  .helpOption('-h, --help', 'output usage information')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
