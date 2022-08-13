c817c31f4d0cf23fa42a910880e92dbbc6eba50b
utkapodsousom <utkapodsousom@gmail.com>
2022-08-13 15:32:45 +0300

    add first test

--- __tests__/readFile.test.js
+++ __tests__/readFile.test.js
@@ -1,5 +1,18 @@
-import { test, expect } from 'jest';
+import { test, expect } from '@jest/globals';
+import fs from 'fs';
+import path from 'path';
+import genDiff from '../bin/index.js';
 
-test('jest should work', () => {
-  expect(2 + 2).toBe(4);
+const getPath = (file) => path.resolve(process.cwd(), '__fixtures__', file);
+const readFile = (file) => fs.readFileSync(getPath(file), 'utf-8');
+
+test('should read file', () => {
+  const data = {
+    'host': 'hexlet.io',
+    'timeout': 50,
+    'proxy': '123.234.53.22',
+    'follow': false
+  };
+  const file = readFile('file1.json');
+  expect(data).toEqual(JSON.parse(file));
 });
